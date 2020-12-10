a19b551aafa1fdc7b4be5e48db63406ef51c44e2
RahulKandoriya <rahulkandoriya@gmail.com>
2020-12-10 15:30:57 +0530

    *Made the dice by using changeNotifier provider. *Added images of the dice faces.

---
+++ lib/app/home/pages/dice.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:roll_my_dice/app/home/pages/roll_dice_page.dart';
+
+class Dice extends StatelessWidget {
+  void updateDices(DiceModel dice) async {
+    dice.generateDiceOne();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> _diceOneImages = [
+      "assets/images/dice1.png",
+      "assets/images/dice2.png",
+      "assets/images/dice3.png",
+      "assets/images/dice4.png",
+      "assets/images/dice5.png",
+      "assets/images/dice6.png",
+    ];
+    final dice = Provider.of<DiceModel>(context);
+    final c = dice.diceOneCount;
+    var img = Image.asset(
+      _diceOneImages[c - 1],
+      gaplessPlayback: true,
+    );
+    return GestureDetector(
+      onTap: () => updateDices(dice),
+      child: img,
+    );
+  }
+}
+
+
--- lib/app/home/pages/roll_dice_page.dart
+++ lib/app/home/pages/roll_dice_page.dart
@@ -1,9 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roll_my_dice/app/home/pages/dice.dart';
 
 class RollDicePage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ChangeNotifierProvider<DiceModel>(
+      child: Dice(),
+      create: (BuildContext context) => DiceModel(),
+    );
+  }
+}
+class DiceModel with ChangeNotifier {
+  int diceOne = 1;
+
+  int get diceOneCount => diceOne;
+
+  void generateDiceOne() {
+    diceOne = Random().nextInt(5) + 1;
+    print("diceOne: $diceOne");
+    notifyListeners();
   }
 }
